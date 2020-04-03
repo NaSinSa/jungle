761d8c33ebb489123efecb3e49a024b7224a0721
NaSinSa <andy.k.oh@hotmail.com>
2020-04-03 00:38:01 +0000

    add admin index

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.admin_auth[:id], password: Rails.configuration.admin_auth[:password]
 
   def show
+    @products = Product.all
+    @categories = Category.all
   end
 end
