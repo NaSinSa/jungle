4f9641c614fcdf08f340c0df8cd77210c6a76694
NaSinSa <andy.k.oh@hotmail.com>
2020-04-07 06:11:18 +0000

    add password length err msg when signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,17 @@ class UsersController < ApplicationController
     if User.find_by_email(user_params[:email]).present?
       flash[:alert] = 'The email you enter already exists!'
       redirect_to '/signup'
+    elsif user_params[:password].length < 2
+        flash[:alert] = 'Password is too short (minimum is 2 characters)'
+        redirect_to '/signup'
     else
       user = User.new(user_params)
         if user.save
-        session[:user_id] = user.id
-        redirect_to '/'
-      else
-        redirect_to '/signup'
-      end
+          session[:user_id] = user.id
+          redirect_to '/'
+        else
+          redirect_to '/signup'
+        end
     end
   end
 
